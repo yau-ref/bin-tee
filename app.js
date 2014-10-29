f12765b938b7a5f0fde102ea160f87aece130697
Yau Ref <yau.ref@gmail.com>
2014-10-29 23:13:17 +0300

    Now quotes can be getted by requeseting /quotes

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var http = require('http');
 var path = require('path');
 
 var routes = require('./routes');
+var quotes = require('./routes/quotes');
 
 var app = express();
 app.set('port', process.env.PORT || 3000);
@@ -15,6 +16,7 @@ app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', routes.index);
+app.get('/quotes/', quotes.all);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,3 @@
 exports.index = function(req, res){
-  res.render('index', { title: 'Quotes' });
-};
+  res.render('index', { title: 'Quotes'})
+}
