2dd73b2507f1178cca705bf830010cb7fd922814
Yau Ref <yau.ref@gmail.com>
2014-10-30 00:10:06 +0300

    Moved js to separate file

--- public/javascripts/actions.js
+++ public/javascripts/actions.js
@@ -1,119 +1,40 @@
-function loadMarks(){
-  user = $("#users option:selected" ).text()
-    $.ajax({
-      url: '/marks/' + user,
-      success: function(result){
-        $('#marks').text("")
-        marks = result.split(";")       
-        $.each(marks, function( index, value ) {
-          $('#marks')
-           .append($('<li class="markLine"></li>')
-           .text(value))
-        })
-        $(".markLine").unbind()
-        $(".markLine").click(editMark)
-      }
-    })
+String.prototype.supplant = function (o) {
+  return this.replace(/{([^{}]*)}/g,
+    function (a, b) {
+      var r = o[b]
+      return typeof r === 'string' || typeof r === 'number' ? r : a
+    }
+  )
 }
 
-function loadUsers(){
-    $.ajax({
-      url: '/users',
-      success: function(result){
-        users = result.split(",")       
-        $.each(users, function( index, value ) {
-          $('#users')
-            .append($("<option></option>")
-              .attr("value", value)
-              .text(value))
-        })
-        loadMarks()
-        $("#lists").css("visibility", "visible")
-      }
-    })
+function makeQuote(quoteData){
+  var template =  '<div class="quote"> \
+                      <p class="text">{text}</p> \
+                      <div class="controls"> \
+                        <a href="#" class="rateUp">+</a> \
+                        <span class="rating">{rating}</span> \
+                        <a href="#" class="rateDown">−</a> \
+                        <p class="info"> \
+                          <span class="quote_date">{date}</span> \
+                          <a href="#" class="quote_id"># {id}</a> \
+                        </p> \
+                      </div> \
+                   </div>'
+  return template.supplant({rating: quoteData.rating, date: quoteData.date, id: quoteData.id}).supplant({text: quoteData.text})
 }
 
-function editMark(e){
-  if (!e.shiftKey) {
-    $("#marks").hide()
-    $("#addmarkForm").show()
-    $("#addMark").text("Изменить")
-    var data = $(this).text().split(': ')
-    $('#subj').attr("readonly",true) 
-    $("#subj").val(data[0])
-    $("#markVal").val(data[1])
-    
-    $("#addMark").unbind()
-    $("#addMark").click(submitMark(true))
-  }else{
-     deleteMark(this)
-  }
-}
-
-function deleteMark(elt){
-  var data = $(elt).text().split(': ')
-  var user = $("#users option:selected" ).text()
-  var subj = data[0]
-  var mark = data[1]
+function loadQuotes(list){
   $.ajax({
-    url: '/marks/',
-    type: 'DELETE',
-    data: {
-      username: user,
-      subject: subj,
-      markValue: mark
-    },
-    success: function(result) {
-      $("#marks").hide()
-      $("#marks").show()
-      loadMarks()
+    url: '/quotes/',
+    success: function(quotes){
+      for(var i in quotes) {
+        $(list).append($(makeQuote(quotes[i])))
+      }
     }
   })
 }
 
 
-
-function submitMark(update){
-  return function(){
-    var user = $("#users option:selected" ).text()
-    var subj = $("#subj").val()
-    var mark = $("#markVal").val()
-
-    $.ajax({
-      url: '/marks/',
-      type: update ? 'PUT' : 'POST',
-      data: {
-        username: user,
-        subject: subj,
-        markValue: mark
-      },
-      success: function(result) {
-        loadMarks()
-      }
-    })
-
-
-    $("#addmarkForm").hide()
-    $("#marks").show()
-    $(this).unbind()
-    $(this).click(showAddForm)
-    $(this).text("+")
-    $('#subj').attr("readonly",false) 
-  }
-}
-
-function showAddForm(){
-  $("#marks").hide()
-  $("#addmarkForm").show()
-  $("#addMark").text("Добавить")
-  $("#addMark").unbind()
-  $("#addMark").click(submitMark(false))  
-}
-
- 
 $(document).ready(function(){
-  $("#showIt").click(loadUsers)
-  $("#users").click(loadMarks)
-  $("#addMark").click(showAddForm)  
-})
 
+})
