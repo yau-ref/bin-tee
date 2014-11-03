7839e2585ee5039eb3df073033f9c27d385983ba
Yau Ref <yau.ref@gmail.com>
2014-11-03 14:41:44 +0300

    Reloading location insted of ajax-loading pages after adding new quote

--- public/javascripts/actions.js
+++ public/javascripts/actions.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
       },
       datatype: 'json', 
       success: function(quotes){
-        loadQuotes('#quotes')
+        location.reload()
         $('#quoteAddForm').hide()
       }
     })   
