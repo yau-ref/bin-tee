b30a2c60624782e0f0c54a9ffbbb3edc74eb7b74
Yau Ref <yau.ref@gmail.com>
2015-01-06 07:35:25 +0300

    Rest through common functions

--- routes/quotes.js
+++ routes/quotes.js
@@ -1,37 +1,22 @@
+var quotes = require('./storageAccess.js');
 var DB_COMMENTS = 2;
 
 exports.all = function(req, res){
-  var redisClient = req.redisClient
-  
-  redisClient.scan(0, function(err, ids){
-    redisClient.mget(ids[1], function(err, quotes){
-      var normalizedArray = quotes.map(function(quote){return JSON.parse(quote)})
-      res.json(normalizedArray)
-    });
-  });
+  quotes.all(req.redisClient, function(quotes){
+    res.json(quotes);
+  });  
 }
 
 exports.top = function(req, res){
-  var redisClient = req.redisClient
-  
-  redisClient.select(1, function(){
-    redisClient.smembers("topQuotes", function(err, ids){
-      redisClient.select(0, function(){
-        redisClient.mget(ids, function(err, quotes){
-          var normalizedArray = quotes.map(function(quote){return JSON.parse(quote)})
-          res.json(normalizedArray)
-        });  
-      });
-    });
+  quotes.top(req.redisClient, function(quotes){
+    res.json(quotes);
   });
 }
 
 exports.byId = function(req, res){
-  var redisClient = req.redisClient
   var quoteId = req.params.id
-  redisClient.get(quoteId, function(err, quote){
-    var normalizedQuote = Array(JSON.parse(quote))
-    res.json(normalizedQuote)
+  quotes.byId(req.redisClient, quoteId, function(quote){
+    res.json(quote);
   });
 }
 
